683db191fbc2425a7d220c27cef142e33305d5c2
casdevlabs <cas737185@gmail.com>
2024-02-13 17:45:30 +0100

    change

--- app/components/dashboard/index.ts
+++ app/components/dashboard/index.ts
@@ -1,7 +1,7 @@
 export { Chart } from "./statistics/chart";
 export { UserCard } from "./user-card";
 export { CourseCard } from "./course-card";
-export { SubscriptionCard } from "./subscription-card";
 export { Notification } from "./notification";
 export { Courses } from "./courses";
 export { Statistics } from "./statistics";
+export { SubscriptionCard } from "./subscription-card";
